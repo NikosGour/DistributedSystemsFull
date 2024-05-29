15f809eaca7e9d6a6671e44bb521ec9916a68bbb
NikosGour <nikosgournak@gmail.com>
2024-05-29 18:05:20 +0300

    Latest

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 IP=curl ifconfig.me
-sed -i "s|BASE_URL=.*|BASE_URL=${IP}|" .env
-docker compose up --build
+
+echo $IP
+# sed -i "s|BASE_URL=.*|BASE_URL=${IP}|" .env
+# docker compose up --build
