ff1a6608edf94edb3c6d421b998f105816d74915
NikosGour <nikosgournak@gmail.com>
2024-05-29 18:15:43 +0300

    Latest

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IP=`curl -s ifconfig.me`
 
-echo "The machine's ip is =${IP}"
+echo "The machine's ip is = ${IP}"
 sed -i "s|^BASE_URL=.*|BASE_URL=${IP}|" .env
 docker compose up --build
